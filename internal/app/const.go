6351f1cfcc121cfc40f7a873bb62b3e93e99afff
kion <kion.code@gmail.com>
2026-02-21 22:09:16 -0500

    Bump app version after project restructuring

--- internal/app/const.go
+++ internal/app/const.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	appVersion                                  = "1.7.3"
+	appVersion                                  = "1.7.4"
 	defaultGitHubRepoUrl                        = "github.com/kion/mbgen"
 	defaultGitHubRepoThemesUrl                  = defaultGitHubRepoUrl + "/themes"
 	defaultGitHubRepoPageContentSamplesUrl      = defaultGitHubRepoUrl + "/content-samples/pages"
